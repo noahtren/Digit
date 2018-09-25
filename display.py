121e39f97b8b27acca10054cb1a7025cc07518c1
noahtren <noahtren@gmail.com>
2018-09-24 23:17:41 -0400

    added functions for generating and displaying data

--- display.py
+++ display.py
@@ -6,18 +6,35 @@ def fill(start, finish, count):
     if (start == finish):
         return np.full(count, start)
     else:
-        return np.arange(start, finish, (finish-start)/count)
+        return np.arange(start, finish, (finish-start)/(count-0.5))
+
 # 10,000 samples per meaning unit
-# each meaning unit will consist of 3 stages
+# each meaning unit will consist of 3 epochs
 # that result in a unique vibration pattern
-t = fill(0, 100, 10000)
-s = np.concatenate((fill(0, 100, 3333), fill(100,100,3333), fill(100,0,3334)))
+def gen_data(epochs):
+    data = []
+    for epoch in epochs:
+        if (epoch == "L"):
+            data = np.append(data, fill(40, 40, 3333))
+        elif (epoch == "H"):
+            data = np.append(data, fill(150,150,3333))
+        elif (epoch == "LH"):
+            data = np.append(data, fill(40,150,3333))
+        elif (epoch == "HL"):
+            data = np.append(data, fill(150,40,3333))
+    data = np.append(data, data[9998])
+    data = (fill(0,100,10000), data)
+    return data
 
-fig, ax = plt.subplots()
-ax.plot(t, s)
+def disp_data(t, s):
+    fig, ax = plt.subplots() 
+    ax.plot(t, s)
+    ax.set(xlabel='Time (% of meaning unit interval)', ylabel='Frequency (Hz)',
+       title='Vibration Speaker Frequency')
+    plt.ylim(35,155)
+    ax.grid()
+    plt.show()
 
-ax.set(xlabel='time (% out of meaning unit)', ylabel='frequency (Hz)',
-       title='vibration speaker frequency')
-ax.grid()
-
-plt.show()
+epochs = ["L", "H", "L"]
+(t,s) = gen_data(epochs)
+disp_data(t,s)
