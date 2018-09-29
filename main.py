e67ab6afaad5b84101280aecabbec243a0a23d7c
noahtren <noahtren@gmail.com>
2018-09-29 15:48:47 -0400

    cleaning up main file

--- main.py
+++ main.py
@@ -8,16 +8,18 @@ def setup():
     # assigning the pin mode to the way that they are
     # displayed on a raspberry pi board, regardless of
     # the specific broadcom chip that is used in a raspi model
-    GPIO.setmode(GPIO.BOARD)
+    GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
     # assign 6 different GPIO pins to output
     # pins = [14, 23, 25, 12, 16, 17]
-    pwm = []
+    pins = [14]
+    pwms = []
     for i in range(0, len(pins)):
-        GPIO.setup(pin[i], GPIO.OUT)
+        GPIO.setup(pins[i], GPIO.OUT)
         # setup pwm object for each pin, and set duty cycle to 50% for square wave
-        pwm[i] = GPIO.PWM(pin[i], 400)
-        pwm[i].dc(50)
+        # test hz
+        pwms[i] = GPIO.PWM(pins[i], 400)
+        pwms[i].start(50)
 
 def run_epoch(pins, data):
     return 1
@@ -26,7 +28,7 @@ pins = [14]
 # string, beginning index
 s = reader.Stream("aaaaaaaaaa", 0)
 # speed in seconds, length (number of devices), step iterator -- currently broken just use 1
-r = reader.Reader(2, 6, 1)
+r = reader.Reader(2, 1, 1)
 
 samples = (r.rate)*2500
 
@@ -34,9 +36,11 @@ while 1:
     status = r.read(s)
     chunk = s.string[s.index:s.index+r.length]
     d = encode.gen_data(chunk, samples)
+    pwms = []
     for state in range(0, samples*4):
         for i in range(0, len(pins)):
-            GPIO.PWM(pins[i], d[state+samples*4*i])
-            time.sleep(10**6)
+            pwms[i] = GPIO.PWM(pins[i], d[state+samples*4*i])
+            pwms[i].start(50)
+            time.sleep(10**-6)
     if status == 0:
         break
