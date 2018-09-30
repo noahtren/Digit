313da7a8ea00397612511fb0d8e257b2a8895e73
noahtren <noahtren@gmail.com>
2018-09-30 11:26:17 -0400

    Removed test file, cleaned up main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import reader
 import encode
 import math
 
-def setup():
+def setup(pins):
     # assigning the pin mode to the way that they are
     # displayed on a raspberry pi board, regardless of
     # the specific broadcom chip that is used in a raspi model
@@ -12,35 +12,38 @@ def setup():
     GPIO.setwarnings(False)
     # assign 6 different GPIO pins to output
     # pins = [14, 23, 25, 12, 16, 17]
-    pins = [14]
     pwms = []
     for i in range(0, len(pins)):
         GPIO.setup(pins[i], GPIO.OUT)
         # setup pwm object for each pin, and set duty cycle to 50% for square wave
         # test hz
-        pwms[i] = GPIO.PWM(pins[i], 400)
+        pwms[i] = GPIO.PWM(pins[i], 100)
         pwms[i].start(50)
-
-def run_epoch(pins, data):
-    return 1
+    return pwms
 
 pins = [14]
+
 # string, beginning index
-s = reader.Stream("aaaaaaaaaa", 0)
+s = reader.Stream("hello_world", 0)
+
 # speed in seconds, length (number of devices), step iterator -- currently broken just use 1
 r = reader.Reader(2, 1, 1)
 
 samples = (r.rate)*2500
 
+d = encode.gen_data(s.string, samples)
+encode.disp_data(d)
+
+pwms = setup(pins)
+time.sleep(1)
+for pwm in pwms:
+    pwm.stop()
+# run the code for an individual vibration speaker
 while 1:
     status = r.read(s)
-    chunk = s.string[s.index:s.index+r.length]
-    d = encode.gen_data(chunk, samples)
-    pwms = []
-    for state in range(0, samples*4):
-        for i in range(0, len(pins)):
-            pwms[i] = GPIO.PWM(pins[i], d[state+samples*4*i])
-            pwms[i].start(50)
-            time.sleep(10**-6)
+    pwms[0].start(50)
+    for state in d:
+        pwms[0].ChangeFrequency(d[state])
+        time.sleep(10**-6)
     if status == 0:
         break
--- test.py
+++
@@ -1,8 +0,0 @@
-# import RPi.GPIO as GPIO
-import time
-import reader
-import encode
-import math
-import main
-
-main.setup()
